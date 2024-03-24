78345c9547097e6da242486c7197dd0a581d21cd
ronphonics <ronphonics@gmail.com>
2024-03-24 00:41:08 -0700

    add if else

---
+++ if_else.py
@@ -0,0 +1,11 @@
+'''Example of if else statement for reference'''
+
+# Enter num function
+num = int(input("Enter a number here: "))
+# If else statement
+if num >0:
+    print("your number is positive")
+elif num <0:
+    print("your number is negative")
+else:
+    print("the number is zero")
