b99fa4417df60ac356d2334e8edf634b436477c8
ronphonics <ronphonics@gmail.com>
2025-09-16 20:53:29 -0700

    Enhance docstring for surname function to clarify arguments

--- python/surname.py
+++ python/surname.py
@@ -1,6 +1,12 @@
 
 def surname(name):
+    """Print a greeting with the given name.
+
+    Args:
+        name (str): The name to include in the greeting.
+    """
     print("Hello, " + name + "!")
+
 
 if __name__ == "__main__":
     surname("Ronnie")
